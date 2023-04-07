4bef9f7ff412d76bcbc3d0a4eddea02ac9fb0d91
viethaihust <vuviethaihust@gmail.com>
2023-04-07 10:11:26 +0700

    add-category

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react'
+import Categories from './Categories'
+import items from './data'
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
 function App() {
+  const [categories] = useState(allCategories)
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Danh mục sản phẩm</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories categories={categories} />
+      </section>
+    </main>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,15 @@
+const menu = [
+  {
+    id: 1,
+    category: 'phone'
+  },
+  {
+    id: 2,
+    category: 'laptop'
+  },
+  {
+    id: 3,
+    category: 'watch'
+  }
+]
+export default menu
