90c4177b8aa965a1fcc9e77551798f54061b5a30
viethaihust <vuviethaihust@gmail.com>
2023-04-11 22:40:55 +0700

    add-desc

--- src/data.js
+++ src/data.js
@@ -4,21 +4,40 @@ const menu = [
     title: 'Samsung Galaxy Note 20',
     category: 'Điện thoại',
     price: 9390000,
-    img: './images/item-1.png'
+    img: './images/item-1.png',
+    desc: 'Sang trọng, cao cấp - Màu sắc độc đáo và cuốn hút, chụp ảnh chuyên nghiệp - Bộ 3 camera hỗ trợ Zoom xa đến 30X'
   },
   {
     id: 2,
     title: 'Laptop Lenovo Gaming Legion 5',
     category: 'Laptop',
     price: 27990000,
-    img: './images/item-2.png'
+    img: './images/item-2.png',
+    desc: 'Chip Core i5-12500H cùng card rời RTX 3060 hiệu năng hàng đầu, chiến game ở mức đồ hoạ cao, xử lý tốt các tác vụ đồ hoạ 2D,3D'
   },
   {
     id: 3,
     title: 'Samsung Galaxy Watch 4',
     category: 'Đồng hồ',
     price: 3990000,
-    img: './images/item-3.png'
+    img: './images/item-3.png',
+    desc: 'Thiết kế cổ điển, màn hình Amoled 1.19 inch hiển thị sắc nét'
+  },
+  {
+    id: 4,
+    title: 'MacBook Pro 2019',
+    category: 'Laptop',
+    price: 17989000,
+    img: '/images/item-4.png',
+    desc: 'MV972 có xung nhịp cơ bản 2.4GHz, tốc độ chạy được TurboBoost lên đến 4.1GHz. Hơn nữa, với dòng sản phẩm này bộ nhớ đệm là 6MB SmartCache, thỏa sức cho người dùng sáng tạo đồ họa cơ bản'
+  },
+  {
+    id: 5,
+    title: 'Samsung Galaxy Z Flip 3 5G',
+    category: 'Điện thoại',
+    price: 8999000,
+    img: '/images/item-5.png',
+    desc: 'Samsung Galaxy Z Flip3 5G dễ dàng lấy lòng phái nữ khi thiết kế của nó được lấy cảm hứng từ hộp đựng phấn trang điểm'
   }
 ]
 export default menu
